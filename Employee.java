f16ba13d073a8c902ce7da1d0c9f77c2e496d27a
salehchadi <91759185+salehchadi@users.noreply.github.com>
2023-05-17 16:08:35 +0300

    Employee.java created

---
+++ Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    private int id ;
+    private String name;
+    public  Employee(int id , String name){
+        this.id = id;
+        this.name = name ;
+    }
+    int getId(){return id;}
+    String getName(){return name;}
+
+}
